3edc2180cddb3b110d0e91999f1a2ac493d8edbd
eric31707 <eric31707@DESKTOP-95TUUP6>
2022-10-24 23:18:38 +0800

    加入專案檔案。

---
+++ Q1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{			
+			int limit = 0;
+			int sum = 1;
+			while (sum + limit < 105)
+			{
+				limit += 1;
+				sum+=limit;
+			}
+			Console.WriteLine(limit);
+		}
+	}
+}
---
+++ Q2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int number = 1;
+			while (number < 300)
+			{
+				number++;
+				if (300 % number == 0)
+				{
+					Console.WriteLine(number);
+				}
+			}
+		}
+	}
+}
